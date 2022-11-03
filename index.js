d0f4616e4864fbc6d2185a52a3f85792a6596825
Bapu <1,bapu.ojha@gmail.com>
2022-11-03 21:22:56 +0530

    Changes in index.js

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const mainContainer = document.querySelector("main");
 const thankYouContainer = document.querySelector(".thankYou");
 const submitBtn = document.querySelector("#submit");
 const buttons = document.querySelectorAll(".numBtn");
-const rateBtn = document.querySelector("#rate");
 
 submitBtn.onclick = function () {
     thankYouContainer.classList.remove("hidden");
     mainContainer.classList.add("hidden");
 }
 
-rateBtn.onclick = () => {
-    thankYouContainer.classList.add("hidden");
-    mainContainer.classList.remove("hidden");
-}
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function () {
